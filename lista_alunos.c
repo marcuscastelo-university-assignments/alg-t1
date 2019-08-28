68066433a58dbbf45cdd118cf61c8fff6f9f0999
marcuscastelo <viniciuscastelo6@hotmail.com>
2019-08-28 15:41:32 -0300

    Implementada função de criar nova lista

--- lista_alunos.c
+++ lista_alunos.c
@@ -1,8 +1,17 @@
 #include "lista_alunos.h"
 
+#include <stdio.h>
+
 struct lista_alunos {
     int quantidade;
     Aluno *primeiro_aluno;
 };
+
+ListaAlunos* lista_criar() {
+    ListaAlunos *lista = (ListaAlunos*) malloc(sizeof(ListaAlunos));
+    lista -> primeiro_aluno = NULL;
+    lista -> quantidade = 0;
+    return lista;
+}
 
 //TODO: Implementar funções definidas no .h
