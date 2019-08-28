3493c01e5552f55ac12617d7b9bd33a1cb5f3b1a
marcuscastelo <viniciuscastelo6@hotmail.com>
2019-08-28 15:15:38 -0300

    O menu deve ficar em loop até que se escolha sair

--- main.c
+++ main.c
@@ -5,49 +5,51 @@
 
 int main(int argc, char const *argv[])
 {
+    while (1) {
+        printf("Menu principal. Escolha uma operação:\n\n");
+        printf("(1) Inserir Registro\n");
+        printf("(2) Remover Registro\n");
+        printf("(3) Imprimir Registros\n");
+        printf("(4) Relatório de Aprovações\n");
+        printf("(5) Horas de estudo (Média)\n");
+        printf("(6) Sair\n");
+        printf(":");
 
-    printf("Menu principal. Escolha uma operação:\n\n");
-    printf("(1) Inserir Registro\n");
-    printf("(2) Remover Registro\n");
-    printf("(3) Imprimir Registros\n");
-    printf("(4) Relatório de Aprovações\n");
-    printf("(5) Horas de estudo (Média)\n");
-    printf("(6) Sair\n");
-    printf(":");
+        int opcao;
+        scanf("%d", &opcao);
 
-    int opcao;
-    scanf("%d", &opcao);
+        switch (opcao)
+        {
+        case 1:
+            //INSERIR REGISTRO
+            break;
 
-    switch (opcao)
-    {
-    case 1:
-        //INSERIR REGISTRO
-        break;
+        case 2:
+            //REMOVER REGISTRO
+            break;
 
-    case 2:
-        //REMOVER REGISTRO
-        break;
+        case 3:
+            //IMPRIMIR REGISTROS
+            break;
 
-    case 3:
-        //IMPRIMIR REGISTROS
-        break;
+        case 4: 
+            //RELATÓRIO DE APROVACAO
+            break;
 
-    case 4: 
-        //RELATÓRIO DE APROVACAO
-        break;
+        case 5:
+            //HORAS DE ESTUDO (MÉDIA)
+            break;
 
-    case 5:
-        //HORAS DE ESTUDO (MÉDIA)
-        break;
-
-    case 6:
-        //SAIR
-        break;
-    
-    default:
-        //OPCAO INVALIDA
-        break;
+        case 6:
+            //SAIR
+            break;
+        
+        default:
+            //OPCAO INVALIDA
+            break;
+        }
     }
+
 
 
     return 0;
