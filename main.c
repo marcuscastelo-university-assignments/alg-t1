9b5a9382cb4766f7d3a2384e62d96efe02bba024
marcuscastelo <viniciuscastelo6@hotmail.com>
2019-09-05 20:18:29 -0300

    corrigindo identação

--- main.c
+++ main.c
@@ -48,6 +48,8 @@ int solicitar_identificador_remocao(ListaAlunos *verifica) {
     }
 }
 
+
+//TODO: formatar melhor a saida das funções.
 int main(int argc, char const *argv[])
 {
     ListaAlunos *lista_alunos;
@@ -60,40 +62,38 @@ int main(int argc, char const *argv[])
 
         switch (opcao)
         {
-        case 1: {
-            Aluno *novo_aluno = solicitar_registro();
-            lista_adicionar(lista_alunos, novo_aluno);
-            break;
-        }
-        
-
-        case 2:{
-            int identificador = solicitar_identificador_remocao(lista_alunos);
-            if(identificador != -1) lista_remover_aluno(lista_alunos, identificador);
-            break;
-        }
+            case 1: {
+                Aluno *novo_aluno = solicitar_registro();
+                lista_adicionar(lista_alunos, novo_aluno);
+                break;
+            }
             
+            case 2:{
+                int identificador = solicitar_identificador_remocao(lista_alunos);
+                if(identificador != -1) lista_remover_aluno(lista_alunos, identificador);
+                break;
+            }
+                
+            case 3:
+                lista_imprimir_alunos(lista_alunos);
+                break;
 
-        case 3:
-            lista_imprimir_alunos(lista_alunos);
-            break;
+            case 4: 
+                //TODO: RELATÓRIO DE APROVACAO ...
+                break;
 
-        case 4: 
-            //TODO: RELATÓRIO DE APROVACAO ...
-            break;
+            case 5:
+                lista_imprimir_tempo_medio_estudo(lista_alunos);
+                break;
 
-        case 5:
-            lista_imprimir_tempo_medio_estudo(lista_alunos);
-            break;
-
-        case 6:
-            lista_liberar(lista_alunos);
-            exit(0);
-            break;
-        
-        default:
-            printf("Opção escolhida é invalida!");
-            break;
+            case 6:
+                lista_liberar(lista_alunos);
+                exit(0);
+                break;
+            
+            default:
+                printf("Opção escolhida é invalida!");
+                break;
         }
     }
 
