4a190364de0a459818112ccc548c5e09c8c65044
daltonsato <54639674+daltonsato@users.noreply.github.com>
2019-09-21 13:03:59 -0300

    Add files via upload
    Comentei os .h das funcoes definidas e adaptei para double pointer

--- aluno.h
+++ aluno.h
@@ -5,10 +5,14 @@ typedef struct aluno Aluno;
 
 //TODO: Comentar as funções
 
+//Funcao que cria o ponteiro do TAD aluno e retorna-o para referencia
 Aluno *aluno_criar(int identificador, float horas_estudo, float nota1, float nota2);
+
 void aluno_atualizar_horas(Aluno*, int horas);
 void aluno_atualizar_nota1(Aluno*, int nota1);
 void aluno_atualizar_nota2(Aluno*, int nota2);
-void aluno_apagar(Aluno*);
+
+//Funcao utilizada para apagar/dar free no ponteiro do TAD aluno
+void aluno_apagar(Aluno**);
 
 #endif
