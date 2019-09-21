fee6d3772f77f942e4f96e8f03947a0786c8bec9
marcuscastelo <viniciuscastelo6@hotmail.com>
2019-09-21 13:03:56 -0300

    Corrigindo double pointer no .h

--- aluno.h
+++ aluno.h
@@ -3,12 +3,10 @@
 
 typedef struct aluno Aluno;
 
-//TODO: Comentar as funções
-
 Aluno *aluno_criar(int identificador, float horas_estudo, float nota1, float nota2);
 void aluno_atualizar_horas(Aluno*, int horas);
 void aluno_atualizar_nota1(Aluno*, int nota1);
 void aluno_atualizar_nota2(Aluno*, int nota2);
-void aluno_apagar(Aluno*);
+void aluno_apagar(Aluno**);
 
 #endif
--- lista.h
+++ lista.h
@@ -5,15 +5,13 @@
 
 typedef struct lista_alunos ListaAlunos;
 
-//TODO: Comentar as funções
-
 ListaAlunos* lista_criar(void);
 void lista_adicionar(ListaAlunos*, Aluno*);
 void lista_remover_aluno(ListaAlunos*, int);
 void lista_imprimir_alunos(ListaAlunos*);
 void lista_imprimir_relatorio(ListaAlunos*);
 void lista_imprimir_tempo_medio_estudo(ListaAlunos*);
-void lista_liberar(ListaAlunos *lista);
+void lista_liberar(ListaAlunos **lista);
 
 int verifica_lista_vazia(ListaAlunos *vazia);
 
